692183739b72136513cb6d9ff48d2ea8a7451bf5
chaxchaptatwo <68698260+chaxchaptatwo@users.noreply.github.com>
2020-07-26 16:36:54 +0200

    Update HomeJavascript.js

--- Javascript/HomeJavascript.js
+++ Javascript/HomeJavascript.js
@@ -3,7 +3,7 @@
   $('.container').hide();
   var image = '<div class="Image"><img class="FlashingImage" src="Images/Transparent/Transparent.png"/></div>';
   $('body').append(image);
-  $('.Image').css({"margin": "auto", "width": "65%", "height": "90vh"})
+  $('.Image').css({"margin": "auto", "width": "65%", "height": "85vh"})
   $('.FlashingImage').css({"width": "100%",	"height": "100%"});
   var i;
   for(i = 0; i < 3; i++)
@@ -11,4 +11,3 @@
     $('.Image').fadeIn(1000).fadeOut(1000);
   };
   $('.container').delay(7000).fadeIn(3000);
-})();
