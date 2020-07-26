295fc01de2fdc4283cdb062bf920e2d169ed1847
chaxchaptatwo <68698260+chaxchaptatwo@users.noreply.github.com>
2020-07-26 15:15:42 +0200

    Update Javascript.js

--- Javascript/Javascript.js
+++ Javascript/Javascript.js
@@ -3,7 +3,7 @@
   $('.container').hide();
   var image = '<div class="Image"><img class="FlashingImage" src="../Images/Transparent/Transparent.png"/></div>';
   $('html').append(image);
-  $('.Image').css({"margin": "auto", "width": "45%", "height": "98%"})
+  $('.Image').css({"margin": "auto", "width": "65%", "height": "98%"})
   $('.FlashingImage').css({"width": "100%",	"height": "100%"});
   var i;
   for(i = 0; i < 3; i++)
