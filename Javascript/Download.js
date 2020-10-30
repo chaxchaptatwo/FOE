70ec0fbb477a35e3608b8ac5e91f144bf4055235
chaxchaptatwo <chax.chapta@gmail.com>
2020-10-30 22:52:47 +0200

    Update Download.js

--- Javascript/Download.js
+++ Javascript/Download.js
@@ -1,8 +1,15 @@
 (function()
 {
   $("a#DownloadNow").click(function(){
-    $("img.DownloadNow").hide();
-    $("img.DownloadNow").attr("src", "../Images/ThankYou.png");
-    $("img.DownloadNow").fadeIn(3000);
+      $("img.DownloadNow").hide();
+      $("img.DownloadNow").attr("src", "../Images/ThankYou.png");
+      $("img.DownloadNow").fadeIn(3000);
+      setTimeout(
+      	function()
+      	{
+        $("img.DownloadNow").hide();
+        $("img.DownloadNow").attr("src", "../Images/Download Here.png");
+        $("img.DownloadNow").fadeIn(3000);
+      	}, 5000);
   });
 }());
