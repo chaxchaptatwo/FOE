4406ef46d55d4739b710244b7f3b89b9e341c3e8
chaxchaptatwo <chax.chapta@gmail.com>
2020-11-01 18:15:02 +0200

    Home page update

--- Javascript/HomeJavascript.js
+++ Javascript/HomeJavascript.js
@@ -18,4 +18,26 @@
       $("ul.NaviList").fadeIn(2000);
       bool = false;
   });
+
+  //Menu Notice
+  setTimeout(function()
+  {
+      $("img#Menu").fadeOut(500).fadeIn(500);
+  }, 2000);
+  setTimeout(function()
+  {
+    if (notclicked)
+    {
+      $("img#Menu").fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500);
+    }
+  }, 7000);
+  setTimeout(function()
+  {
+    if (notclicked)
+    {
+      $("img#Menu").fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500);
+    }
+  }, 12000);
+
+
 }());
