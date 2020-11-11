b577194af3b89bc6733f4723ddec27002792864d
chaxchaptatwo <chax.chapta@gmail.com>
2020-11-11 12:23:42 +0200

    Update Download.js

--- Javascript/Download.js
+++ Javascript/Download.js
@@ -1,14 +1,21 @@
 (function()
 {
+  //Download ThankYou
+  download = "../Images/Download here.png";
+  thankYou = "../Images/ThankYou.png";
+  //Family Business EP
+  fCover = "../Images/FB/Front Cover.jpg";
+  bCover = "../Images/FB/Back Cover.jpg";
+
   $("a#DownloadNow").click(function(){
       $("img.DownloadNow").hide();
-      $("img.DownloadNow").attr("src", "../Images/ThankYou.png");
+      $("img.DownloadNow").attr("src", thankYou);
       $("img.DownloadNow").fadeIn(3000);
       setTimeout(
         function()
         {
           $("img.DownloadNow").hide();
-          $("img.DownloadNow").attr("src", "../Images/Download here.png");
+          $("img.DownloadNow").attr("src", download);
           $("img.DownloadNow").fadeIn(3000);
         }, 5000);
   });
@@ -16,7 +23,7 @@
   $("a.previous").click(function(error){
       error.preventDefault();
       $("img.BckFrntCover").hide();
-      $("img.BckFrntCover").attr("src", "../Images/FB/Front Cover.jpg");
+      $("img.BckFrntCover").attr("src", fCover);
       $("img.BckFrntCover").fadeIn(1000);
       $("a.previous").css({"background-color": "#000000"});
       $("a.next").css({"background-color": "#8B0000"});
@@ -24,7 +31,7 @@
   $("a.next").click(function(error){
       error.preventDefault();
       $("img.BckFrntCover").hide();
-      $("img.BckFrntCover").attr("src", "../Images/FB/Back Cover.jpg");
+      $("img.BckFrntCover").attr("src", bCover);
       $("img.BckFrntCover").fadeIn(1000);
       $("a.previous").css({"background-color": "#8B0000"});
       $("a.next").css({"background-color": "#000000"});
